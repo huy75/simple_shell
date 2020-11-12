cfa83e7ee5771208da23abffa8f5b04c7de0a507
huy75 <huy.nguyen66@gmail.com>
2020-11-12 09:11:27 +0000

    fix remove unused struct list descriptor

--- shell.h
+++ shell.h
@@ -1,17 +1,18 @@
 #ifndef __SHELL__
 #define __SHELL__
+
+#include <fcntl.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <sys/resource.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/wait.h>
-#include <fcntl.h>
 #include <string.h>
 #include <dirent.h>
 #include <signal.h>
+#include <unistd.h>
 
 #define BUFSIZE 1024
 #define ENV extern char **environ
@@ -20,7 +21,6 @@
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of the string
  * @next: points to the next node
  */
 typedef struct list_s
@@ -28,8 +28,5 @@ typedef struct list_s
 	char *str;
 	struct list_s *next;
 } list_t;
-
-char *_getenv(const char *name);
-list_t *add_node_end(list_t **head, const char *str);
 
 #endif /* __SHELL__ */
