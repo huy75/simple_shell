c1cd698c07e9d349714d6803946fde8f59958c26
huy75 <huy.nguyen66@gmail.com>
2020-11-12 08:40:40 +0000

    first commit

---
+++ shell.c
@@ -0,0 +1,45 @@
+#include "practice.h"
+
+/**
+ * main - simple shell
+ * Return: 0 or 98 if failed
+ */
+int main(void)
+{
+	char *av[1024], *buffer, *token;
+	char delimiters[] = " \t\r\n\v\f";
+	size_t size = BUFSIZE;
+	int status, idx;
+	pid_t pid;
+
+	buffer = malloc(sizeof(char) * BUFSIZE);
+        if (!buffer)
+                perror("ERROR: Unable to allocate buffer\n"), exit(98);
+
+	while (1)
+	{
+		write(STDOUT_FILENO, PROMPT, 3);
+		if (getline(&buffer, &size, stdin) == -1)
+			perror("ERROR: could not read from input\n"), exit(98);
+
+		token = strtok(buffer, delimiters);
+
+		for (idx = 0; token; idx++)
+		{
+			av[idx] = token;
+			token = strtok(NULL, delimiters);
+		}
+		av[idx] = NULL;
+
+		pid = fork();
+		if (pid == 0)
+		{
+			if (execve(av[0], av, NULL) == -1)
+				perror("sh");
+		}
+		else
+			wait(&status);
+	}
+	free(buffer);
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,35 @@
+#ifndef __SHELL__
+#define __SHELL__
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+
+#define BUFSIZE 1024
+#define ENV extern char **environ
+#define PROMPT "$"
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ */
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
+
+char *_getenv(const char *name);
+list_t *add_node_end(list_t **head, const char *str);
+
+#endif /* __SHELL__ */
