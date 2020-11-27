3405295991e0461484bd6338a3e547f1f1c917c2
huy75 <huy.nguyen66@gmail.com>
2020-11-27 12:46:46 +0000

    fix getline handles spaces

--- _getline.c
+++ _getline.c
@@ -39,6 +39,8 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 	*lineptr = malloc(sizeof(char) * 1);
 	while ((re = read(STDIN_FILENO, &tmp, 1)) > 0)
 	{
+		if (tmp == ' ')
+			continue;
 		if (tmp == '\n')
 			*(*lineptr + i) = '\0';
 		else
